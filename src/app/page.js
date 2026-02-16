83fef38ae652e9adcb4f77858ca014e1bdd0e2bb
Saima Khan <saimakhancsedu@gmail.com>
2026-02-17 02:55:28 +0600

    trying github shared repo working

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <p>Hello skm </p>
         <h1>Secure Exam </h1>
+        <h3>Branch 2</h3>
       </main>
     </div>
   );
